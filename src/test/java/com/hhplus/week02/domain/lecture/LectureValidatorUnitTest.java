44f8a73e1b1480bd36965745b7ff4ae15864cfc3
nashs789 <nashs789@naver.com>
2024-10-01 01:20:17 +0900

    🧪Test: 강의 아이디 유효성 검사 Validator

--- src/test/java/com/hhplus/week02/domain/lecture/LectureValidatorUnitTest.java
+++ src/test/java/com/hhplus/week02/domain/lecture/LectureValidatorUnitTest.java
@@ -1,4 +1,4 @@
-package com.hhplus.week02.domain.member;
+package com.hhplus.week02.domain.lecture;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,26 +8,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MemberValidatorUnitTest {
+class LectureValidatorUnitTest {
 
-    private final MemberValidator memberValidator = new MemberValidator();
+    private final LectureValidator lectureValidator = new LectureValidator();
 
-    @DisplayName("유효한 유저 ID 값 테스트")
+    @DisplayName("유효한 강의 ID 값 테스트")
     @ParameterizedTest(name = "{0} 이 유효한 ID 인지 테스트")
     @ValueSource(longs = {1L, 100L, 100_000L, Long.MAX_VALUE})
     public void isValidUserId(Long userId) {
-        memberValidator.validator(userId);
+        lectureValidator.validator(userId);
 
         assertTrue(true);
     }
 
-    @DisplayName("유효하지 유저 않은 ID 값 테스트")
+    @DisplayName("유효하지 강의 않은 ID 값 테스트")
     @ParameterizedTest(name = "{0} 은 유효하지 않은 아이디")
     @ValueSource(longs = {-1L, 0L})
     @NullSource
     public void isInvalidUserId(Long userId) {
-        assertThrows(MemberException.class, () -> {
-            memberValidator.validator(userId);
+        assertThrows(LectureException.class, () -> {
+            lectureValidator.validator(userId);
         });
     }
 }
